4d73853031f918dcface448ea8cf6f9b431cb885
crvwler <aaababuji@gmail.com>
2024-10-17 07:16:51 +0100

    MOD Views and Routing

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is the cart page</h1>
+  <div class="p-6 bg-gray-100 min-h-screen">
+    <div class="max-w-lg mx-auto bg-white rounded-xl shadow-md p-8">
+      <h1 class="text-2xl font-bold mb-4">Welcome to the Cart</h1>
+      <p class="text-gray-600 mb-6">View lessons added to cart here.</p>
+      <button class="bg-blue-500 text-white px-4 py-2 rounded">
+        Check Out
+      </button>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script>
+export default {
+  name: "Lessons",
+};
+</script>
